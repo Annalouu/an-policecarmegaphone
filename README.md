ccf4c782769ce76b6379cf3dceedce8210929d83
Annalouu <112778590+Annalouu@users.noreply.github.com>
2023-02-04 04:51:20 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # an-policecarmegaphone
  
+A small script i wrote to add the ability to have a megaphone in the cop cars
+
+- You can either run this by itself or get the megaphonepolice.lua and add it to your qb-policejob client and add it to your fxmanifest 
+
+# Usage: 
+ -Hold LeftShift and keep holding it and then use your push to talk to speak loudly.
+
+- if you want to add more jobs to the script just modify line 13 from this 
+  ```
+  if DoesEntityExist(vehicle) and vehicleClass == 18 and PlayerJob == 'police' then
+  ```
+  to this for example we are going to add the bsco job to it.
+  ```
+  if DoesEntityExist(vehicle) and vehicleClass == 18 and PlayerJob == 'police' or PlayerJob == 'bcso' then
+  ```
---
+++ megaphonepolice.lua
@@ -0,0 +1,42 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local inacopcar = false
+
+
+CreateThread(function()
+        while true do
+            local ped = PlayerPedId()
+            if IsPedInAnyVehicle(ped) then
+                if not inacopcar then
+                    local PlayerJob = QBCore.Functions.GetPlayerData().job.name
+                    local vehicle = GetVehiclePedIsIn(ped)
+                    local vehicleClass = GetVehicleClass(vehicle)
+                    if DoesEntityExist(vehicle) and vehicleClass == 18 and PlayerJob == 'police' then
+                        StartListeningForControlmegaphone()
+                        inacopcar = true
+                    end
+                end
+            else
+                inacopcar = false
+                listentokey = false
+            end
+            Wait(1000)
+        end
+end)
+
+
+function StartListeningForControlmegaphone()
+	listentokey = true
+	InputDisabled = false
+	CreateThread(function()
+		while listentokey do
+			if IsControlJustPressed(0, 61) then 
+				exports["pma-voice"]:overrideProximityRange(30.0, true)
+                QBCore.Functions.Notify('Megaphone on', 'success')
+            elseif IsControlJustReleased(0, 61) then 
+                exports["pma-voice"]:clearProximityOverride()
+                QBCore.Functions.Notify('Megaphone off', 'error')
+			end
+			Wait(1)
+		end
+	end)
+end
