83ecae8281cadf110f170d7b0b5f392cb9f37e89
JericoFX <56100605+JericoFX@users.noreply.github.com>
2023-02-06 13:16:29 -0300

    Better Indent
    this could be better using SetPlayerData as client event

--- megaphonepolice.lua
+++ megaphonepolice.lua
@@ -1,30 +1,32 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local inVeh = false
 
+
 AddEventHandler('gameEventTriggered', function(name, args)
-if name == "CEventNetworkPlayerEnteredVehicle" then
-    local PlayerJob = QBCore.Functions.GetPlayerData().job.name
+  if name == "CEventNetworkPlayerEnteredVehicle" then
     local vehicle = tonumber(args[2])
     local vehicleClass = GetVehicleClass(vehicle)
-    if DoesEntityExist(vehicle) and vehicleClass == 18 and PlayerJob == 'police' then
-        inVeh = true
+    if DoesEntityExist(vehicle) and vehicleClass == 18 then
+      inVeh = true
     end
-end
+  end
 end)
 
 --https://cookbook.fivem.net/2020/01/06/using-the-new-console-key-bindings/
 RegisterCommand('+Megaphone', function()
-if inVeh then
+  local PlayerJob = QBCore.Functions.GetPlayerData().job.name
+  if inVeh and PlayerJob == "police" then
     exports["pma-voice"]:overrideProximityRange(30.0, true)
     QBCore.Functions.Notify('Megaphone on', 'success')
-end
+  end
 end, false)
 
 RegisterCommand('-Megaphone', function()
- if inVeh then
-	exports["pma-voice"]:clearProximityOverride()
-	QBCore.Functions.Notify('Megaphone off', 'error')
- end
+  local PlayerJob = QBCore.Functions.GetPlayerData().job.name
+  if inVeh and PlayerJob == "police" then
+    exports["pma-voice"]:clearProximityOverride()
+    QBCore.Functions.Notify('Megaphone off', 'error')
+  end
 end, false)
 
 RegisterKeyMapping('+Megaphone', 'Talk on the Megaphone', 'keyboard', 'LSHIFT')
