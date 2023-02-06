ddf8fd60d5f9b1f02f3856b34ebe0142edddc389
JericoFX <56100605+JericoFX@users.noreply.github.com>
2023-02-06 13:13:30 -0300

    Delete while true loops

--- megaphonepolice.lua
+++ megaphonepolice.lua
@@ -1,41 +1,30 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local inacopcar = false
+local inVeh = false
 
-
-CreateThread(function()
-        while true do
-            local ped = PlayerPedId()
-            if IsPedInAnyVehicle(ped) then
-                if not inacopcar then
-                    local PlayerJob = QBCore.Functions.GetPlayerData().job.name
-                    local vehicle = GetVehiclePedIsIn(ped)
-                    local vehicleClass = GetVehicleClass(vehicle)
-                    if DoesEntityExist(vehicle) and vehicleClass == 18 and PlayerJob == 'police' then
-                        StartListeningForControlmegaphone()
-                        inacopcar = true
-                    end
-                end
-            else
-                inacopcar = false
-                listentokey = false
-            end
-            Wait(1000)
-        end
+AddEventHandler('gameEventTriggered', function(name, args)
+if name == "CEventNetworkPlayerEnteredVehicle" then
+    local PlayerJob = QBCore.Functions.GetPlayerData().job.name
+    local vehicle = tonumber(args[2])
+    local vehicleClass = GetVehicleClass(vehicle)
+    if DoesEntityExist(vehicle) and vehicleClass == 18 and PlayerJob == 'police' then
+        inVeh = true
+    end
+end
 end)
 
-
-function StartListeningForControlmegaphone()
-	listentokey = true
-	CreateThread(function()
-		while listentokey do
-			if IsControlJustPressed(0, 61) then 
-				exports["pma-voice"]:overrideProximityRange(30.0, true)
-                QBCore.Functions.Notify('Megaphone on', 'success')
-            elseif IsControlJustReleased(0, 61) then 
-                exports["pma-voice"]:clearProximityOverride()
-                QBCore.Functions.Notify('Megaphone off', 'error')
-			end
-			Wait(1)
-		end
-	end)
+--https://cookbook.fivem.net/2020/01/06/using-the-new-console-key-bindings/
+RegisterCommand('+Megaphone', function()
+if inVeh then
+    exports["pma-voice"]:overrideProximityRange(30.0, true)
+    QBCore.Functions.Notify('Megaphone on', 'success')
 end
+end, false)
+
+RegisterCommand('-Megaphone', function()
+ if inVeh then
+	exports["pma-voice"]:clearProximityOverride()
+	QBCore.Functions.Notify('Megaphone off', 'error')
+ end
+end, false)
+
+RegisterKeyMapping('+Megaphone', 'Talk on the Megaphone', 'keyboard', 'LSHIFT')
